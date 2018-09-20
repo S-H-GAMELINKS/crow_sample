c3c8b121782fb1797b7972c64c5930cff9485ae8
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-20 13:02:51 +0900

    import index.html

--- test_app.cpp
+++ test_app.cpp
@@ -1,10 +1,20 @@
 #include "crow.h"
+#include "fstream"
+#include <sstream>
+#include <string>
 
 int main() {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/")([](){
-      return "<h1>Hello, World!</h1>";
+      std::ifstream file("index.html");
+      std::stringstream strstream;
+      strstream << file.rdbuf();
+      file.close();
+
+      //ファイルの内容をstringに入れる
+      std::string data( strstream.str() );
+      return data;
   });
 
   app.port(3000).multithreaded().run();
