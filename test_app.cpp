70a36fef984c385101bb054e226fd941efa6108f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-20 17:28:35 +0900

    add loadView func

--- test_app.cpp
+++ test_app.cpp
@@ -3,18 +3,23 @@
 #include <sstream>
 #include <string>
 
+std::string loadView(const std::string& path){
+  std::ifstream file(path.c_str());
+  std::stringstream strstream;
+  
+  strstream << file.rdbuf();
+  file.close();
+
+  std::string data( strstream.str() );
+  
+  return data;
+}
+
 int main() {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/")([](){
-      std::ifstream file("index.html");
-      std::stringstream strstream;
-      strstream << file.rdbuf();
-      file.close();
-
-      //ファイルの内容をstringに入れる
-      std::string data( strstream.str() );
-      return data;
+    return loadView("index.html");
   });
 
   app.port(3000).multithreaded().run();
